2e2427c36f88e23abe6986d517803ae70b75c76e
martinperezcanada <148330505+martinperezcanada@users.noreply.github.com>
2024-06-12 00:52:55 +0200

    Actualizar configuraciones y dependencias

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 import { defineConfig } from 'astro/config';
 import svelte from "@astrojs/svelte";
-
 import react from "@astrojs/react";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [svelte(), react()]
+  integrations: [svelte(), react()],
+  output: "server",
+  adapter: vercel()
 });
